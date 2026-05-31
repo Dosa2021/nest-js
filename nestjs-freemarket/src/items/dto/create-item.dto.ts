4c13ca6e3b2d3a219fadc2a62f26b8fa108ea458
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-05-31 17:36:14 +0900

    class-validator導入

--- nestjs-freemarket/src/items/dto/create-item.dto.ts
+++ nestjs-freemarket/src/items/dto/create-item.dto.ts
@@ -1,5 +1,24 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @MaxLength(40)
+  @IsNotEmpty()
   name: string = '';
+
+  @IsInt()
+  @Min(1)
   price: number = 0;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
   description?: string;
 }
