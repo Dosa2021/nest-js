e3d9097299bfc5a9d8f84b03bfd037a112758ecc
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-25 12:28:40 +0900

    create機能作成

--- nestjs-freemarket/src/items/items.controller.ts
+++ nestjs-freemarket/src/items/items.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import type { Item } from './items.model';
 
 @Controller('items')
 export class ItemsController {
@@ -8,5 +9,23 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON SALE',
+    };
+
+    return this.itemsService.create(item);
   }
 }
